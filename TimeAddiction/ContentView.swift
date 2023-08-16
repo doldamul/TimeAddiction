92109cf1b21fd7e2f6ff7a1ae125df3e8558156e
doldamul <junha0504@gmail.com>
2023-08-16 12:56:54 +0900

    Initial Commit

---
+++ TimeAddiction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeAddiction
+//
+//  Created by 돌다물 on 8/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeAddiction/TimeAddictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeAddictionApp.swift
+//  TimeAddiction
+//
+//  Created by 돌다물 on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeAddictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
